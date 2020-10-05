e2f7543131079cfd63e02ccc9f3d6df19a680330
Javier <javier2000asr@gmail.com>
2020-10-05 02:25:23 -0300

    se le agnade un menú con tres opciones de captura
    aun falta crear excepciones para la op 3

--- Captura de json/fecha.py
+++ Captura de json/fecha.py
@@ -1,4 +1,40 @@
 import datetime
+from datetime import timedelta
+import time
+
+
+def menu():
+    men =True
+    while men:
+        now = datetime.datetime.now()#Extraigo la fecha del día Ej: 3 09 2020
+        print("""
+        Hola! Desea extraer datos del día de: 
+        
+        A) Hoy 
+        B) Ayer 
+        C) Hace x días atrás
+
+        """)
+        op = input('->')
+
+        if op=='a' or op=='A':
+            
+            return now #Extraigo la fecha del día Ej: 3 09 2020 y lo retorno
+
+        elif op=='b' or op=='B':
+            
+            ayer = now + timedelta(days = -1)#resto un dia
+            return ayer #retorno el resultado
+
+        elif op=='c' or op=='C':
+
+            dsms = int(input('Indique el numero de días que el quiere restar a la fecha actual \n\n->'))#dias menos
+            new_date = now - timedelta(days = dsms) #resto el numero de dias
+            return new_date
+        else:
+            print('\n\n<---Error en el ingreso de datos--->\n\n')
+            time.sleep(2)
+            pass
 
 
 def SCD(d): #suma 0 en el principio de un dia en caso de que este sea menor a 10
@@ -22,7 +58,7 @@ def SCM(m): #suma 0 en el principio de un mes en caso de que este sea menor a 10
 
 def date():
 
-    a    = datetime.datetime.now() #Extraigo la fecha del día Ej: 3 09 2020
+    a    = menu()
     d    = a.day
     m    = a.month
     agno = a.year
@@ -40,4 +76,3 @@ def hr(): #retorna un formato de hora para el guardado del archivo
     a   = datetime.datetime.now()
     hor = (f'{a.hour}_{a.minute}')
     return hor
-
